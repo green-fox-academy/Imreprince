1cee398c87aea943ad3f52cdac1d3a8b5f0cf683
Ruzics Imre <ruzicsimre@gmail.com>
2021-10-20 16:48:21 +0200

    Add new files.

---
+++ week-02/day-3/com.greenfoxacademy/src/helloWorld.java
@@ -0,0 +1,8 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        /*A konzolra (terminálra) való kiíratás (printelés)
+        Hello World*/
+
+        System.out.println("Hello World!");
+    }
+}
