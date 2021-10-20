1ba61b1f5bc7bf50299ac2edba5b13fc981a5e96
Ruzics Imre <ruzicsimre@gmail.com>
2021-10-20 19:45:58 +0200

    Add new files.

---
+++ week-02/day-3/test-project/src/com/greenfoxacademy/print/HelloOthers.java
@@ -0,0 +1,9 @@
+package com.greenfoxacademy.print;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Helló Eszter!");
+        System.out.println("Helló Maris!");
+        System.out.println("Helló Józsi!");
+    }
+}
