d995aba6bae34ae8c24828c3f6b79f11f7948cad
Ruzics Imre <ruzicsimre@gmail.com>
2021-10-20 15:19:54 +0200

    Add new files.

---
+++ week-02/day-3/src/print/HelloWorld.java
@@ -0,0 +1,10 @@
+package print;
+
+/*A konzolra (terminálra) való kiíratás (printelés)
+        Hello World*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
