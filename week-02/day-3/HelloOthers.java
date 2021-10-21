30f00eca2b47a1dd6b2ead6e9ff6a43a15e3e634
Ruzics Imre <ruzicsimre@gmail.com>
2021-10-21 16:27:25 +0200

    Add new file.

---
+++ week-02/day-3/HelloOthers.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.expressionsandcontrolflow;
+
+//public class HelloOthers {
+//  public static void main(String[] args) {
+//    // Üdvözöld 3 osztálytársad 3 sorban kiiratva a nevüket, ehhez hasonlóan:
+//    //
+//    // Helló Eszter!
+//    // Helló Maris!
+//    // Helló Józsi!
+//  }
+//}
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Helló Eszter!");
+        System.out.println("Helló Maris!");
+        System.out.println("Helló Józsi!");
+    }
+}
