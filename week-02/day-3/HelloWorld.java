b50711f2916f4fee718ccdde56d9b12d3a95f750
Ruzics Imre <ruzicsimre@gmail.com>
2021-10-21 15:31:30 +0200

    Add new file.

---
+++ week-02/day-3/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.print;
+
+//public class HelloWorld {
+//  public static void main(String[] args) {
+//    // Kiírja a konzolra, hogy "Hello, World!"
+//    System.out.println("Hello, World!");
+//  }
+//}
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- week-02/day-3/test-project/src/com/greenfoxacademy/print/HelloOthers.java
+++
@@ -1,9 +0,0 @@
-package com.greenfoxacademy.print;
-
-public class HelloOthers {
-    public static void main(String[] args) {
-        System.out.println("Helló Eszter!");
-        System.out.println("Helló Maris!");
-        System.out.println("Helló Józsi!");
-    }
-}
