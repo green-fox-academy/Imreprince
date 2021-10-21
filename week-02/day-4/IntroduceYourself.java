92da093cf0f9731a2c088c015a15e6183c9734a5
Ruzics Imre <ruzicsimre@gmail.com>
2021-10-21 20:56:31 +0200

    New add file.

---
+++ week-02/day-4/IntroduceYourself.java
@@ -0,0 +1,24 @@
+package com.greenfoxacademy.expressions_and_controlflow;
+
+//public class IntroduceYourself {
+//  public static void main(String[] args) {
+//    // Írj egy programot, ami kiír rólad pár részletet a konzolra
+//    // Minden egyes részletet új sorba írja a program:
+//    //  - Neved
+//    //  - Korod
+//    //  - Magasságod méterben kifejezve (tört érték)
+//    //
+//    //  Példa kimenet (output):
+//    //  John Doe
+//    //  31
+//    //  1.87
+//  }
+//}
+
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Ruzics Imre");
+        System.out.println("51");
+        System.out.println("1.82");
+    }
+}
