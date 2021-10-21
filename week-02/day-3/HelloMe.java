5d1695926d887184a2f6cd90cc4ec698c3c3a149
Ruzics Imre <ruzicsimre@gmail.com>
2021-10-21 16:01:43 +0200

    Add new file.

---
+++ week-02/day-3/HelloMe.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.expressionsandcontrolflow;
+
+//public class HelloMe {
+//  public static void main(String[] args) {
+//    // Úgy módosítsd a programot, hogy téged üdvözöljön a Világ helyett
+//    System.out.println("Hello, World!");
+//  }
+//}
+
+public class HelloMe {
+    public static void main(String[] args) {
+        System.out.println("Hello Ruzics Imre!");
+    }
+}
