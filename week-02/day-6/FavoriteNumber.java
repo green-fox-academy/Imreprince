9934979b3c922aba3b084bf938c0eb6ef6aff716
Ruzics Imre <ruzicsimre@gmail.com>
2021-10-23 13:21:39 +0200

    Add new file.

---
+++ week-02/day-6/FavoriteNumber.java
@@ -0,0 +1,25 @@
+package com.greenfoxacademy.expressions_and_control_flow;
+
+public class FavoriteNumber {
+
+    public static void main(String[] args) {
+
+        //public class FavoriteNumber {
+        //  public static void main(String[] args) {
+        //    int favoriteNumber = 8;
+        //    // Írd át a fenti számot a kedvenc számoddá
+        //    // És utána írasd ki efféle képpen: "A kedvenc számom: 8"
+        //    // Kiíratáskor használd fel a favoriteNumber változót
+        //  }
+        //}
+
+        int favoriteNumber = 8;
+
+        favoriteNumber = 3;
+        System.out.println("A kedvenc számom:" + " " + favoriteNumber);
+
+
+
+    }
+
+}
