96d87c1732ff2732fda3c796f50b8e1c4de3d6b8
Ruzics Imre <ruzicsimre@gmail.com>
2021-10-23 15:22:26 +0200

    Add new file:DefineBasicInfo

---
+++ week-02/day-6/DefineBasicinfo.java
@@ -0,0 +1,31 @@
+package com.greenfoxacademy.expressions_and_control_flow;
+
+public class DefineBasicinfo {
+
+    public static void main(String[] args) {
+
+
+    // Hozz létre pár dolgot változóként és utána írasd ki az értékeiket:
+    // - A neved karakterláncként (string)
+    // - Korod egész számként (integer)
+    // - Magasságod méterben törtszámként (float / double)
+    // - Házas vagy-e logikai értékként (boolean)
+
+
+    String myName = "R" + "u"+"z"+"i"+"c"+"s"+" "+"I"+"m"+"r"+"e";
+        System.out.println("Nevem:"+ myName);
+
+         Integer myJahres = 51;
+        System.out.println("Életkorom:" + myJahres);
+
+        double myTall = 1.82;
+        System.out.println("Magasságom:" + myTall);
+
+        boolean myMarried = true;
+        System.out.println("Házas vagyok:" + myMarried);
+
+
+
+
+    }
+}
