3cb4cf64dc747814a45a19d4a410963844d4ac2b
Ruzics Imre <ruzicsimre@gmail.com>
2021-10-24 12:09:00 +0200

    Add new IWontCheatOnTheExams file

---
+++ week-02/day-7/IWontCheatOnTheExams.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.expressions_and_control_flow;
+
+public class IWontCheatOnTheExams {
+
+    public static void main(String[] args) {
+
+        // Írj egy programot ami 100-szor kiírja:
+        // "Nem fogok csalni az a vizsgán!"
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Nem fogok csalni a vizsgán!");
+        }
+    }
+}
