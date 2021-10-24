900d78237c436915405c1dc1aace7b3cc8ab25e1
Ruzics Imre <ruzicsimre@gmail.com>
2021-10-24 12:41:04 +0200

    Add new PrintEven file.

---
+++ week-02/day-7/PrintEven.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.expressions_and_control_flow;
+
+public class PrintEven {
+
+    public static void main(String[] args) {
+
+        // Írj egy programot, ami kiírja az összes páros számot 0 és 500 között
+
+        int i = 0;
+        while (i < 502) {
+            System.out.println(i);
+            i += 2;
+        }
+    }
+}
